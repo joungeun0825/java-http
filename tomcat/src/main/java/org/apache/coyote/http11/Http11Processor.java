2f146787a997112a459428c2fbe5a6c76993309a
인포텍 <joungeun0825@gmail.com>
2025-03-06 16:58:19 +0900

    feat: generateResponseBody 생성

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,7 +5,9 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
@@ -27,8 +29,25 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
              final var outputStream = connection.getOutputStream()) {
 
+            String[] header = br.readLine().split(" ");
+            String method = header[0];
+            String path = header[1].split(" ")[0];
+
+            String response = generateResponseBody(method, path);
+
+
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private String generateResponseBody(String method, String path){
+        if(method.equals("GET") && path.equals("/")){
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -38,10 +57,13 @@ public class Http11Processor implements Runnable, Processor {
                     "",
                     responseBody);
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-        } catch (IOException | UncheckedServletException e) {
-            log.error(e.getMessage(), e);
+            return response;
         }
+
+        if(method.equals("GET") && path.equals("/index.html")){
+
+        }
+
+        return "";
     }
 }
